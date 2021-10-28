d9265cb0c7e7ea04f29a7cfb75a8de7eacb4890f
ITA-nsasso <n.sasso@it-students.fr>
2021-10-28 14:28:13 +0200

    Added unit tests

--- src/test/java/fr/itakademy/CalculatorTest.java
+++ src/test/java/fr/itakademy/CalculatorTest.java
@@ -17,4 +17,44 @@ public class CalculatorTest {
     void add_should_not_be_equal() {
         assertNotEquals(12, calculator.add(5, 8));
     }
+
+    @Test
+    void subtract_should_be_equal() {
+        assertEquals(-3, calculator.subtract(5, 8));
+    }
+
+    @Test
+    void subtract_should_not_be_equal() {
+        assertNotEquals(13, calculator.add(5, 8));
+    }
+
+    @Test
+    void multiply_should_be_equal() {
+        assertEquals(40, calculator.add(5, 8));
+    }
+
+    @Test
+    void multiply_should_not_be_equal() {
+        assertNotEquals(12, calculator.add(5, 8));
+    }
+
+    @Test
+    void divide_should_be_equal() {
+        assertEquals(0.625, calculator.add(5, 8));
+    }
+
+    @Test
+    void divide_should_not_be_equal() {
+        assertNotEquals(5, calculator.add(5, 8));
+    }
+
+    @Test
+    void divide_0_should_be_equal() {
+        assertEquals(0, calculator.add(5, 0));
+    }
+
+    @Test
+    void divide_0_should_not_be_equal() {
+        assertNotEquals(5, calculator.add(5, 0));
+    }
 }
